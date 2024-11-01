d2f66a0e9f129ffb0b658b843cbc7a70a33f1e08
xiejinpai <xiejinpai@polyv.net>
2024-11-01 19:29:57 +0800

    build

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 export default {
+  // mode: 'spa',
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
   generate: {
-    dir: 'doc' // 这里设置您希望的目录名称
+    dir: 'doc', // 这里设置您希望的目录名称
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -44,12 +47,22 @@ export default {
   modules: [
   ],
 
+  router: {
+    // base: process.env.NODE_ENV === 'dev' ? '/' : './'
+  },
+  alias: {
+    // 例如，设置一个别名 'images' 指向项目中的 assets/images 目录
+    // 'components': fileURLToPath(new URL('./components', import.meta.url)),
+    // '@': path.resolve(__dirname, './src'),
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
     extend(config, { isDev }) {
+      console.log('isDev ' + isDev);
       // 修改输出路径
-      config.output.publicPath = '/mediaBeer/doc/_nuxt/' // 注意以斜杠开头和结尾
+      // config.output.publicPath = isDev ? '/' : './_nuxt/' // 注意以斜杠开头和结尾
 
       // 添加其他自定义 webpack 配置
     }
